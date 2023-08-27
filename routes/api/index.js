0b3b7d331992e56802a937df8d50790a26983e33
Christina2208 <christinalf22@gmail.com>
2023-08-27 17:28:40 -0400

    exporting router

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+//requiring express + routes
+const router = require("express").Router();
+const categoryRoutes = require("./category-routes");
+const productRoutes = require("./product-routes");
+const tagRoutes = require("./tag-routes");
+
+//routes
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
+
+//exporting
+module.exports = router;
