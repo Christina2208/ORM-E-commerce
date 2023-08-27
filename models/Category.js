170131bafb4aba409f5352a28c2fee90c85a2eb4
Christina2208 <christinalf22@gmail.com>
2023-08-27 17:51:02 -0400

    created model for category

---
+++ models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  },
+  // category name column
+  category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+  }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+//exporting
+module.exports = Category;
