5ae272f00e3025c98e0acb4e788e91b81a689ae3
Christina2208 <christinalf22@gmail.com>
2023-08-27 15:01:07 -0400

    created product/tag ids

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,28 +2,28 @@ const { Tag } = require('../models');
 
 const tagData = [
   {
-    tag_name: 'gold',
+    tag_name: 'gold', //1
   },
   {
-    tag_name: 'silver',
+    tag_name: 'silver', //2
   },
   {
-    tag_name: 'lip',
+    tag_name: 'lip', //3
   },
   {
-    tag_name: 'skin',
+    tag_name: 'skin', //4
   },
   {
-    tag_name: 'hair',
+    tag_name: 'hair', //5
   },
   {
-    tag_name: 'leather',
+    tag_name: 'leather', //6
   },
   {
-    tag_name: 'canvas',
+    tag_name: 'canvas', //7
   },
   {
-    tag_name: 'casual',
+    tag_name: 'casual', //8
   },
 ];
 
